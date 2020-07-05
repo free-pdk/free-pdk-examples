1bdb3b8c0f37f16c56d5cf5f97aca7f10a77d243
serisman <github@serisman.com>
2020-07-05 00:23:32 -0500

    Add Serial_HelloWorld example

---
+++ Serial_HelloWorld/main.c
@@ -0,0 +1,46 @@
+/*
+  Serial_HelloWorld
+
+  Transmits "Hello World" @ ~115200 baud, and repeats once a second.
+*/
+
+#include <pdk/device.h>
+#include "sysclock.h"
+#include "serial.h"
+#include "delay.h"
+
+// Note: serial.h assumes TX is on PA7, and uses timer2 (TM2) interrupts for timing.
+
+void interrupt(void) __interrupt(0) {
+  if (INTRQ & INTRQ_TM2) {      // TM2 interrupt request?
+    INTRQ &= ~INTRQ_TM2;        // Mark TM2 interrupt request processed
+    serial_irq_handler();       // Process next Serial Bit
+  }
+}
+
+// Main hardware initialization.
+inline void setup() {
+  serial_setup();               // Initialize Serial engine
+  __engint();                   // Enable global interrupts
+}
+
+// Main processing loop.
+inline void loop() {
+  serial_println("Hello World!");
+  _delay_ms(1000);
+}
+
+// Main program.
+void main() {
+  setup();
+  while(1) {
+    loop();
+  }
+}
+
+// Startup code - Setup/calibrate system clock.
+unsigned char _sdcc_external_startup(void) {
+  FREE_PDK_INIT_SYSCLOCK();
+  FREE_PDK_CALIBRATE_SYSCLOCK(TARGET_VDD_MV);
+  return 0;
+}
