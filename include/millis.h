b98ee03d944d3c0cb79eefd5316c086d96a050fb
serisman <github@serisman.com>
2020-07-09 01:28:42 -0500

    Improve comments

--- include/millis.h
+++ include/millis.h
@@ -3,11 +3,13 @@
 
 #include <stdint.h>
 
-volatile uint8_t _ticks;
-volatile uint32_t _millis;                       // Number of elapsed milliseconds
+volatile uint8_t _ticks;                        // Increments every 32 uS
+volatile uint32_t _millis;                      // Number of elapsed milliseconds (rolls over every 49.71 days)
 
 void millis_setup() {
   // Setup timer16 (T16) interrupt to tick every 32 uS
+  // Note: This assumes the internal IHRC oscillator is running and has been calibrated to the normal 16MHz and not some weird frequency.
+  // The system clock can still be set to use a divider, or to use ILRC or EOSC (as long as IHRC is still enabled).
   T16M = (uint8_t)(T16M_CLK_IHRC | T16M_CLK_DIV1 | T16M_INTSRC_8BIT);
   T16C = 0;
   INTEN |= INTEN_T16;
@@ -15,16 +17,16 @@ void millis_setup() {
 
 uint32_t millis() {
   INTEN &= ~INTEN_T16;                          // Disable T16 (read of 32 bit value _millis is non-atomic)
-  uint32_t currentMillis = _millis;
+  uint32_t currentMillis = _millis;             // Read _millis while T16 interrupt is off (otherwise it might be modified on us!)
   INTEN |= INTEN_T16;                           // Re-enable T16
   return currentMillis;
 }
 
 void millis_irq_handler() {
-  if (_ticks++ == 31) { // Should really be 31.25, but close enough?
+  if (_ticks++ == 31) {                         // This should really be 31.25, but close enough?
     _ticks = 0;
     _millis++;
   }
 }
 
-#endif //__SERIAL_H__
+#endif //__MILLIS_H__
