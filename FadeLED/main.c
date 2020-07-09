33e2d0850ecb7029e078aae523bfcdd9e38c0f19
serisman <github@serisman.com>
2020-07-09 00:03:53 -0500

    Add support for TM2 8-bit PWM for devices that don't have 11-bit PWMG1

--- FadeLED/main.c
+++ FadeLED/main.c
@@ -9,35 +9,60 @@
 #include "auto_sysclock.h"
 #include "delay.h"
 
-// LED is placed on Port A, Pin 4, which is also PG1PWM (current sink configuration)
-#define LED_PIN             4
+// Define which type of PWM to use
+#if defined(PWMG1CUBL)            // Full 11-bit PWM:   PFS154,PMS154C
+  #define PWM_11_FULL
+#elif defined(PWMGCUBL)           // Basic 11-bit PWM:  PFS173,PMS152
+  #define PWM_11_BASIC
+#else                             // 8-bit PWM:         PFS172,PMS15A,PMS150C,PMC171B
+  #define PWM_8
+#endif
+
+// Define which Pin the LED is on (current sink configuration)
+#if defined(PWM_8)
+  #define LED_PIN             3   // LED is placed on Port A, Pin 3, which is also TM2PWM
+#else
+  #define LED_PIN             4   // LED is placed on Port A, Pin 4, which is also PG1PWM
+#endif
 
 int16_t brightness = 0;           // How bright the LED is
 int8_t fadeAmount = 5;            // How many points to fade the LED by
 
-// Main program.
+// Main program
 void main() {
+
   // Initialize hardware
   PAC |= (1 << LED_PIN);          // Set LED_PIN as output (all pins are input by default)
 
+#if defined(PWM_8)
+  TM2B = 0x00;                    // Clear the LED PWM duty value
+  TM2C = (uint8_t)(TM2C_INVERT_OUT | TM2C_MODE_PWM | TM2C_OUT_PA3 | TM2C_CLK_IHRC);
+  TM2S = 0x00;                    // No pre-scaler, 8-bit
+#elif defined(PWM_11_FULL)
   PWMG1DTL = 0x00;                // Clear the LED PWM duty value
   PWMG1DTH = 0x00;
-
-#if defined(PWMG1CUBL)
   PWMG1CUBL = 0xFF;               // Setup PWM upper bound
   PWMG1CUBH = 0x00;
   PWMG1C = (uint8_t)(PWMG1C_ENABLE | PWMG1C_INVERT_OUT | PWMG1C_OUT_PA4 | PWMG1C_CLK_IHRC);
+  PWMG1S = 0x00;                  // No pre-scaler
 #else
+  PWMG1DTL = 0x00;                // Clear the LED PWM duty value
+  PWMG1DTH = 0x00;
   PWMGCUBL = 0x00;                // Setup PWM upper bound
   PWMGCUBH = 0xFF;
   PWMGCLK = (uint8_t)(PWMGCLK_PWMG_ENABLE | PWMGCLK_CLK_IHRC);
   PWMG1C = (uint8_t)(PWMG1C_INVERT_OUT | PWMG1C_OUT_PWMG1 | PWMG1C_OUT_PA4);
 #endif
 
-  // Main processing loop.
+  // Main processing loop
   while (1) {
+#if defined(PWM_8)
+    TM2B = brightness;            // Set the LED PWM duty value
+#else
     PWMG1DTH = brightness;        // Set the LED PWM duty value
+#endif
 
+    // Adjust the brightness
     brightness += fadeAmount;
     if (brightness + fadeAmount <= 0 || brightness > 255) {
       fadeAmount = -fadeAmount;
@@ -48,7 +73,7 @@ void main() {
   }
 }
 
-// Startup code - Setup/calibrate system clock.
+// Startup code - Setup/calibrate system clock
 unsigned char _sdcc_external_startup(void) {
 
   // Initialize the system clock (CLKMD register) with the IHRC, ILRC, or EOSC clock source and correct divider.
