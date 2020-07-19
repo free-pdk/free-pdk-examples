be9aa5499fafb1639d2f8d12d05af57e1a311e5e
serisman <github@serisman.com>
2020-07-19 17:58:49 -0500

    Fix PWM values

--- FadeLED/main.c
+++ FadeLED/main.c
@@ -9,6 +9,8 @@
 #include "auto_sysclock.h"
 #include "delay.h"
 
+#define PWM_MAX               255
+
 // Define which type of PWM to use
 #if defined(PWMG1CUBL)            // Full 11-bit PWM:   PFS154,PMS154C
   #define PWM_11_FULL
@@ -36,17 +38,17 @@ void main() {
   TM2C = (uint8_t)(TM2C_INVERT_OUT | TM2C_MODE_PWM | TM2C_OUT_PA3 | TM2C_CLK_IHRC);
   TM2S = 0x00;                    // No pre-scaler, 8-bit
 #elif defined(PWM_11_FULL)
+  PWMG1CUBL = PWM_MAX << 5;       // Set the PWM upper bound (lower 3 bits)
+  PWMG1CUBH = PWM_MAX >> 3;       // (upper 5 bits)
   PWMG1DTL = 0x00;                // Clear the LED PWM duty value
   PWMG1DTH = 0x00;
-  PWMG1CUBL = 0x00;               // Setup PWM upper bound
-  PWMG1CUBH = 0xFF;
   PWMG1C = (uint8_t)(PWMG1C_ENABLE | PWMG1C_INVERT_OUT | PWMG1C_OUT_PA4 | PWMG1C_CLK_IHRC);
   PWMG1S = 0x00;                  // No pre-scaler
 #else
+  PWMGCUBL = PWM_MAX << 5;        // Set the PWM upper bound (lower 3 bits)
+  PWMGCUBH = PWM_MAX >> 3;        // (upper 5 bits)
   PWMG1DTL = 0x00;                // Clear the LED PWM duty value
   PWMG1DTH = 0x00;
-  PWMGCUBL = 0x00;                // Setup PWM upper bound
-  PWMGCUBH = 0xFF;
   PWMGCLK = (uint8_t)(PWMGCLK_PWMG_ENABLE | PWMGCLK_CLK_IHRC);
   PWMG1C = (uint8_t)(PWMG1C_INVERT_OUT | PWMG1C_OUT_PWMG1 | PWMG1C_OUT_PA4);
 #endif
@@ -56,21 +58,23 @@ void main() {
     uint8_t fadeValue;
 
     // Fade in from min to max in increments of 5
-    for (fadeValue = 0; fadeValue < 255; fadeValue += 5) {
+    for (fadeValue = 0; fadeValue < PWM_MAX; fadeValue += 5) {
       #if defined(PWM_8)
         TM2B = fadeValue;         // Set the LED PWM duty value
       #else
-        PWMG1DTH = fadeValue;     // Set the LED PWM duty value
+        PWMG1DTL = fadeValue << 5;  // Set the LED PWM duty value (lower 3 bits)
+        PWMG1DTH = fadeValue >> 3;  // (upper 8 bits)
       #endif
       _delay_ms(30);              // wait for 30 milliseconds to see the dimming effect
     }
 
     // Fade out from max to min in increments of 5
-    for (fadeValue = 255; fadeValue > 0; fadeValue -= 5) {
+    for (fadeValue = PWM_MAX; fadeValue > 0; fadeValue -= 5) {
       #if defined(PWM_8)
         TM2B = fadeValue;         // Set the LED PWM duty value
       #else
-        PWMG1DTH = fadeValue;     // Set the LED PWM duty value
+        PWMG1DTL = fadeValue << 5;  // Set the LED PWM duty value (lower 3 bits)
+        PWMG1DTH = fadeValue >> 3;  // (upper 8 bits)
       #endif
       _delay_ms(30);              // wait for 30 milliseconds to see the dimming effect
     }
