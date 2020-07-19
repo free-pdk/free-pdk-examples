5250f19c137fa1999faa5c4323cdb2e7c32dc470
serisman <github@serisman.com>
2020-07-19 18:19:47 -0500

    Pull in upstream fixes

--- include/pdk/device/periph/misc.h
+++ include/pdk/device/periph/misc.h
@@ -46,11 +46,15 @@ __sfr __at(MISC_ADDR)         _misc;
 #define MISC_WATCHDOG_64K_ILRC       (2 << MISC_WATCHDOG_BIT0)
 #define MISC_WATCHDOG_256K_ILRC      (3 << MISC_WATCHDOG_BIT0)
 
+#define MISC_LVR_ENABLE              0x00
 #define MISC_LVR_DISABLE             (1 << MISC_LVR_DISABLE_BIT)
+
+#define MISC_FAST_WAKEUP_DISABLE     0x00
 #define MISC_FAST_WAKEUP_ENABLE      (1 << MISC_FAST_WAKEUP_ENABLE_BIT)
 
 #if defined(__PDK_HAS_LCD)
   #define MISC_LCD_ENABLE_BIT          4
+  #define MISC_LCD_DISABLE             0x00
   #define MISC_LCD_ENABLE              (1 << MISC_LCD_ENABLE_BIT)
 #endif
 
