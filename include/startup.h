913d4a1ab861fcd42a7d60c37164ab679d9a6496
Josh Shabtai <josh@shabtai.ca>
2024-01-28 13:49:39 -0500

    Added comment for startup macro

--- include/startup.h
+++ include/startup.h
@@ -1,3 +1,5 @@
+// The name of the function that sdcc uses for startup changed.
+// This macro assigns the correct name to STARTUP_FUNCTION based on the sdcc version.
 #if __SDCC_REVISION >= 13762
 #define STARTUP_FUNCTION __sdcc_external_startup
 #else
